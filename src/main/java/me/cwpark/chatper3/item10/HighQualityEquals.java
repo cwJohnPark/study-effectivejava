e75545260043f9a3a081a5c9338b030c5b6e6f4a
cwJohnPark <pierrotcw@gmail.com>
2020-09-28 06:05:49 +0900

    Add item10 Non-nullity

---
+++ src/main/java/me/cwpark/chatper3/item10/HighQualityEquals.java
@@ -0,0 +1,15 @@
+package me.cwpark.chatper3.item10;
+
+/*
+ * A recipe for a high-quality equals method
+ */
+public class HighQualityEquals {
+
+    // 1. Use the == operator to check if the argument is a reference to this object
+
+    // 2. Use the instanceof operator to check if the argument has the correct type
+
+    // 3. Cast the argument to the correct type
+
+    // 4. Check if "significant" field of the argument matches the corresponding field of this object
+}
---
+++ src/main/java/me/cwpark/chatper3/item10/NonNullity.java
@@ -0,0 +1,20 @@
+package me.cwpark.chatper3.item10;
+
+/*
+ * Non-nullity
+ * All objects must be unequal to null
+ */
+public class NonNullity {
+
+    @Override public boolean equals(Object o) {
+        // if (o == null) return false; // This test is unnecessary
+
+        // instanceof operator is specified to return false if its first operand is null
+        if (!(o instanceof NonNullity))
+            return false;
+        NonNullity n = (NonNullity) o;
+
+        //....
+        return true;
+    }
+}

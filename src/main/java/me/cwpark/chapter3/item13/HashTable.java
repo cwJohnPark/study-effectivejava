fd3b90d79a52e7daab874143afa056847173cb4c
cwJohnPark <pierrotcw@gmail.com>
2020-10-27 06:08:49 +0900

    Clone Iteratively HashTable

---
+++ src/main/java/me/cwpark/chapter3/item13/HashTable.java
@@ -0,0 +1,55 @@
+package me.cwpark.chapter3.item13;
+
+public class HashTable implements Cloneable {
+
+    private Entry[] buckets;
+
+    private static class Entry {
+        final Object key;
+        Object value;
+        Entry next;
+
+        public Entry(Object key, Object value, Entry next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        // If the list is long, this could cause a stack overflow
+        Entry deepCopy_recursively() {
+            return new Entry(key, value, next == null ? null : next.deepCopy_recursively());
+        }
+
+        // Iteratively copy the linked list headed by this Entry
+        public Entry deepCopy() {
+            Entry result = new Entry(key, value, next);
+            for (Entry p = result; p.next != null; p = p.next)
+                p.next = new Entry(p.next.key, p.next.value, p.next.next);
+            return result;
+        }
+    }
+
+    // Broken clone method - results in shared mutable state!
+    public HashTable clone_broken() throws CloneNotSupportedException {
+        try {
+            HashTable result = (HashTable) super.clone();
+            result.buckets = buckets.clone();
+            return result;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+
+    @Override public HashTable clone() {
+        try {
+            HashTable result = (HashTable) super.clone();
+            result.buckets = new Entry[buckets.length];
+            for (int i = 0; i < buckets.length; i++)
+                if (buckets[i] != null)
+                    result.buckets[i] = buckets[i].deepCopy();
+            return result;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+}
